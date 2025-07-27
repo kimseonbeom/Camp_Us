917f19923cff5a756edb26cd9a4cf957b0846c1e
User <User@DESKTOP-OH8U662>
2025-07-27 11:20:05 +0900

    menu-1st

--- com.campUs.application/src/main/java/com/camp_us/controller/TestController.java
+++ com.campUs.application/src/main/java/com/camp_us/controller/TestController.java
@@ -1,25 +1,21 @@
 package com.camp_us.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/background")
+public class TestController {
 
-	import org.springframework.stereotype.Controller;
-	import org.springframework.web.bind.annotation.GetMapping;
-	import org.springframework.web.bind.annotation.ModelAttribute;
-	import org.springframework.web.bind.annotation.PathVariable;
-	import org.springframework.web.bind.annotation.RequestMapping;
+	@GetMapping("")
+	public void background(@ModelAttribute("color") String color) {
+	}
 
-	@Controller
-	@RequestMapping("/background")
-	public class TestController {
-		
-		@GetMapping("")
-		public void background(@ModelAttribute("color") String color) {}
-		
-		
-		@GetMapping("/{color}")
-		public String backgroundPath(@ModelAttribute("color")
-								   @PathVariable("color")  
-		 						   String color) {
-			return "background";
-		}
-		}
+	@GetMapping("/{color}")
+	public String backgroundPath(@ModelAttribute("color") @PathVariable("color") String color) {
+		return "background";
+	}
+}
